4848c10119823498c48b91f85e5ffa3ee9d22211
Rex2002 <frahm.benjamin@gmx.de>
2023-01-12 16:08:05 +0100

    removed deprecated SettingsFragment

--- app/src/main/java/de/dhbw/tinderpol/BottomSettingsFragment.kt
+++ app/src/main/java/de/dhbw/tinderpol/BottomSettingsFragment.kt
@@ -15,10 +15,6 @@ class BottomSettingsFragment : BottomSheetDialogFragment() {
 
     private val binding get() = _binding!!
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
