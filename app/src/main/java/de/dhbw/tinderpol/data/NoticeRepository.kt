10a327e9db9cce7c2d36ade5030fbe043c59b570
ArtInLines <linesinart@gmail.com>
2023-01-19 09:56:43 +0100

    Additional change for merge

--- app/src/main/java/de/dhbw/tinderpol/data/NoticeRepository.kt
+++ app/src/main/java/de/dhbw/tinderpol/data/NoticeRepository.kt
@@ -9,7 +9,7 @@ class NoticeRepository {
         private val remoteDataSource = RemoteNoticesDataSource()
         private var lastUpdated: LocalDate = LocalDate.of(2000,1,1)
 
-        suspend fun fetchAllNotices(): List<Notice> {
+        suspend fun fetchNotices(): List<Notice> {
             if (lastUpdated != LocalDate.now()) {
                 val remoteNotices = remoteDataSource.fetchNotices().getOrDefault(listOf())
                 Log.i("API-Req", remoteNotices.size.toString())
