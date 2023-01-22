7dfa1560ba8caae35d0fb292fbf2374408b9762f
JakobPK <s211200@student.dhbw-mannheim.de>
2023-01-22 20:07:34 +0100

    Map Activity further, the exact thing we wanted doesnt work over the maps API, so we have to work with less accurate data. Also function is in comment because its missing some parts of implementation

--- app/src/main/java/de/dhbw/tinderpol/MapsActivity.kt
+++ app/src/main/java/de/dhbw/tinderpol/MapsActivity.kt
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import de.dhbw.tinderpol.databinding.ActivityMapsBinding
+import de.dhbw.tinderpol.data.Notice
 import de.dhbw.tinderpol.util.OnSwipeTouchListener
 import com.google.android.material.R.drawable.*
 
@@ -39,10 +40,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    /*
+     * this is probably a lot harder and less efficient than it has to be
+     * but i think the API only lets you use your own location, a pre saved one or you add one
+     * while youre on the map. Therefore a few countries will be hardcoded
+    */
+
+    /*fun getLo(){
+        if(AF){lo = 33.927125}
+        else if(EG){lo = 26.820190}
+        else if(AR){lo = -38.416097}
+        else if(AU){lo = -25.274022}
+        else if(BE){lo = 50.8504500}
+        else if(BG){lo = 42.733883}
+        else if(CN){lo = 19.7699300}
+        else if(DE){lo = 51.165691}
+        else if(EU){lo = 50,02447}
+        else if(RU){lo = 51.8590500}
+        return lo
+         }
+
+
+    fun getLan(){
+        if(AF){lan = 67.721655}
+        else if(EG){lan = 30.798712}
+        else if(AR){lan = -63.616672}
+        else if(AU){lan = 133.775392}
+        else if(BE){lan = 4.3487800}
+        else if(BG){lan = 25.485830}
+        else if(CN){lan = -90.4959500}
+        else if(DE){lan = 10.451526}
+        else if(EU){lan = 9,9319}
+        else if(RU){lan = 58.2213600}
+        return lan
+    }*/
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val lo = 7.5
-        val lan = 8.9
+        val lo = 0.0 //getLo()
+        val lan = 0.0 // getLan()
         val location = LatLng(lo,lan)
         mMap.addMarker(MarkerOptions().position(location).title("Former Location"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
