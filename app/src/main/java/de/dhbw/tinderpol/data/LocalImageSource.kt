26e44db5733818c1b4b9ec59b82b1eeacf51de6e
Rex2002 <frahm.benjamin@gmx.de>
2023-01-29 15:06:52 +0100

    fixes bugs with notice_ids being not uniform and socketExceptions

--- app/src/main/java/de/dhbw/tinderpol/data/LocalImageSource.kt
+++ app/src/main/java/de/dhbw/tinderpol/data/LocalImageSource.kt
@@ -5,6 +5,7 @@ import de.dhbw.tinderpol.SDO
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.*
+import java.net.SocketException
 import java.net.URL
 
 class LocalImageSource {
@@ -19,7 +20,7 @@ class LocalImageSource {
 
                     if (!notice.imgs.isNullOrEmpty()) {
                         notice.imgs!!.forEachIndexed {index, img ->
-                            val file = File(dir,id + "_$index")
+                            val file = File(dir,id + "_" + index.toString())
 
                             //skipping existing files because notices are never updated
                             if (!file.exists()) {
@@ -36,14 +37,19 @@ class LocalImageSource {
                                     inputStream.close()
                                     val response = outputStream.toByteArray()
 
-                                    val fos = FileOutputStream(File(dir, id + "_$index"))
+                                    val fos = FileOutputStream(file)
                                     fos.write(response)
                                     fos.close()
                                 } catch (e: FileNotFoundException) {
                                     Log.e("LocalImageSource",
-                                        "encountered FileNotFoundException while getting image $index for notice ${notice.id}"
+                                        "encountered FileNotFoundException while getting image $index for notice ${notice.id}, \n $img"
                                     )
                                     //no exception handling because intended behaviour is that a file is simply not created when an exception occurs
+                                }catch (e : SocketException){
+                                    Log.e("LocalImageSource",
+                                    "encountered SocketException while saving image $index for notice ${notice.id} to disk")
+                                    //no exception handling because intended behaviour is that a file is simply not created when an exception occurs
+
                                 }
                             }
                         }
@@ -52,7 +58,7 @@ class LocalImageSource {
                 }
                 var fileNames: Array<String> = dir.list() ?: arrayOf()
                 val numberFiles: Int = fileNames.size
-                fileNames = fileNames.filter { !noticeIds.contains(it.substring(0,10)) }.toTypedArray()
+                fileNames = fileNames.filter { !noticeIds.contains(it.split("_")[0] + "_" + it.split("_")[1]) }.toTypedArray()
                 val numberOldFiles: Int = fileNames.size
                 fileNames.forEach {
                     File(dir,it).delete()
@@ -66,17 +72,22 @@ class LocalImageSource {
         suspend fun loadImages(dir: File) {
             withContext(Dispatchers.IO) {
                 val fileNames: Array<String> = dir.list() ?: arrayOf()
+                // each filename has the format year_interPolId_imgNo
 
                 fileNames.forEach {
+                    Log.i("localImageSource", "fileName: $it")
                     val image = ByteArray(90000)
-                    val noticeId: String = it.substring(0,4) + "/" + it.substring(5,10)
-                    val imgIndex: Int = it.substring(11).toInt()
+                    val splitName: List<String> = it.split("_")
+                    val noticeId: String = splitName[0] + "/" + splitName[1]
+                    val imgIndex: Int = splitName[2].toInt()
                     val inputStream = FileInputStream(File(dir, it))
                     inputStream.read(image)
                     inputStream.close()
-                    if (SDO.localImages[noticeId] == null)
+                    if (SDO.localImages[noticeId] == null){
                         SDO.localImages[noticeId] = mutableListOf()
-
+                        Log.i("localImageSource", "created empty mutableList for id $noticeId")
+                    }
+                    Log.i("localImageSource", "SDO.localImages[noticeId]: $noticeId")
                     SDO.localImages[noticeId]!!.add(imgIndex, image)
                 }
             }
